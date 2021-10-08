8e789949e1193766835f330bb3cbf821044ecbf9
Daniel <dhcgn@users.noreply.github.com>
2021-10-08 14:19:19 +0200

    can encode

--- avifencodergui.lib/JobManager.cs
+++ avifencodergui.lib/JobManager.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -45,12 +46,62 @@ namespace avifencodergui.lib
 
         private static async Task<ExecuteImageOperationResult> ExecuteImageOperationAsync(Job job)
         {
-            await Task.Delay(1000);
+            var filename = GetFileName(job);
+            var arguments = GetArguments(job);
+
+            var r = await RunProcessAsync(filename, arguments);
 
             return new ExecuteImageOperationResult()
             {
                 State = Job.JobStateEnum.Done
             };
+        }
+
+        private static string GetArguments(Job job)
+        {
+            return $"--jobs 16 --speed 6 \"{job.FilePath}\" \"{Path.Combine(new FileInfo(job.FilePath).DirectoryName,job.FileName)}.avif\"";
+        }
+
+        private static string GetFileName(Job job)
+        {
+            return ExternalAvifRessourceHandler.EcoderFilePath;
+        }
+
+        // TODO Error Handling and output
+        static Task<(int returnCode, string output)> RunProcessAsync(string fileName, string arguments)
+        {
+            var tcs = new TaskCompletionSource<(int returnCode, string output)>();
+
+            var process = new Process
+            {
+                // EnableRaisingEvents = true,
+                StartInfo = { 
+                    FileName = fileName, 
+                    Arguments = arguments,
+                    WindowStyle= ProcessWindowStyle.Hidden,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                },
+                EnableRaisingEvents = true,
+            };
+
+            
+            process.Exited += (sender, args) =>
+            {
+                string line = "";
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    line += process.StandardOutput.ReadLine() + Environment.NewLine;
+                }
+
+                tcs.SetResult((process.ExitCode, line));
+                process.Dispose();
+            };
+
+            process.Start();
+
+            return tcs.Task;
         }
     }
 
